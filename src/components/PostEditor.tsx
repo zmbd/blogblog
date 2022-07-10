a7d49da2b9318a586c615579a8c335079fa64f65
zmbd <mcypaszh@gmail.com>
2022-07-10 18:31:24 +0300

    Editor menu bar

---
+++ src/components/PostEditor.tsx
@@ -0,0 +1,45 @@
+import { Editor, EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import React, { useCallback, useRef, useState } from "react";
+
+import { AiOutlineBold } from "react-icons/ai";
+import EditorMenubar from "./EditorMenubar";
+
+import "../EditorMenu.css";
+
+const PostEditor = () => {
+  const editor: Editor | null = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World!</p>",
+  });
+
+  return (
+    <>
+      <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box w-11/12 max-w-5xl">
+          <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="font-bold text-lg">Post Customize Tool</h3>
+          <div className="w-full flex flex-col border border-black">
+            <div
+              id="button-group"
+              className="w-full h-9 flex flex-row justify-start items-center rounded-t-lg text-3xl border-b border-black"
+            >
+              <EditorMenubar editor={editor} />
+            </div>
+            <div className="outline-none px-2 py-6">
+              <EditorContent editor={editor} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostEditor;
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { db } from "../firebase/firebase";
 import { UserProps } from "../propTypes";
 import Spinner from "../components/Spinner";
-import CustomizePost from "../components/CustomizePost";
+import PostEditor from "../components/PostEditor";
 
 const Admin = () => {
   const [posts, setPosts] = useState<any>();
@@ -44,31 +44,6 @@ const Admin = () => {
       setUser(user);
     }
   }, []);
-
-  const toggleCustomizePostModal = (): ReactElement => {
-    return (
-      <>
-        <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-        <div className="modal">
-          <div className="modal-box relative">
-            <label
-              htmlFor="my-modal-3"
-              className="btn btn-sm btn-circle absolute right-2 top-2"
-            >
-              ✕
-            </label>
-            <h3 className="text-lg font-bold">
-              Congratulations random Internet user!
-            </h3>
-            <p className="py-4">
-              Youve been selected for a chance to get one year of subscription
-              to use Wikipedia for free!
-            </p>
-          </div>
-        </div>
-      </>
-    );
-  };
 
   useEffect(() => {
     const getPosts = async () => {
@@ -130,7 +105,7 @@ const Admin = () => {
               >
                 add post
               </label>
-              <CustomizePost />
+              <PostEditor />
             </div>
           </div>
           <div className="w-full h-auto flex justify-center my-20">
