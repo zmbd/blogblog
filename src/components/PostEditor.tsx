b8f8b725b85e1a408d6bfe2d034d1ce0a4ff9984
zmbd <mcypaszh@gmail.com>
2022-07-11 12:21:56 +0300

    publishing post, with some hardcoded data, will fix later

--- src/components/PostEditor.tsx
+++ src/components/PostEditor.tsx
@@ -4,12 +4,28 @@ import React, { useCallback, useRef, useState } from "react";
 import EditorMenubar from "./EditorMenubar";
 
 import "../EditorMenu.css";
+import { doc, addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 const PostEditor = () => {
   const editor: Editor | null = useEditor({
     extensions: [StarterKit],
     content: "<p>Hello World!</p>",
   });
+
+  const publishPost = async () => {
+    const docRef = await addDoc(collection(db, "posts"), {
+      authorLabel: "John Doe",
+      imgUrl:
+        "https://images.unsplash.com/photo-1586880244406-556ebe35f282?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80",
+      name: "Custom generated post",
+      pathname: "/article/custom-generated-post",
+      post: editor?.getHTML(),
+      writtenBy: "John Doe",
+      writtenOn: new Date(),
+    });
+    console.log(docRef.id);
+  };
 
   return (
     <>
@@ -23,6 +39,7 @@ const PostEditor = () => {
             ✕
           </label>
           <h3 className="font-bold text-lg">Post Customize Tool</h3>
+          <div className="w-full h-auto flex flex-col justify-start items-center"></div>
           <div className="w-full flex flex-col border border-black">
             <div
               id="button-group"
@@ -33,6 +50,12 @@ const PostEditor = () => {
             <div className="outline-none px-2 py-6">
               <EditorContent editor={editor} />
             </div>
+          </div>
+          <div className="modal-action">
+            <button onClick={() => publishPost()} className="btn btn-success">
+              Publish
+            </button>
+            <button className="btn">Cancel</button>
           </div>
         </div>
       </div>
