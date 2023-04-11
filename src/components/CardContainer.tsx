e9e0c79748b181cc65e67f71e4c69732b6c57727
zmbd <mcypaszh@gmail.com>
2023-04-11 19:25:02 +0300

    [PKP23-22]: fix eslint errors

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,17 +1,16 @@
-import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import {
-  MediumBreakpointContext,
-  ScreenContext,
-} from "../context/screenContext";
-import useWindowDimensions from "../hooks/useWindowDimensions";
-import { propTypes } from "../propTypes";
-import CardContainerContent from "./CardContainerContent";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { MediumBreakpointContext } from '../context/screenContext';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+import { propTypes } from '../propTypes';
+import CardContainerContent from './CardContainerContent';
 
 const CardContainer = (props: propTypes) => {
   const { post, order_key, contentsLoaded, forKey, specialLayout } = props;
 
-  const { isLowerMediumBreak }: any = useContext(MediumBreakpointContext);
+  const { isLowerMediumBreak }: { [key: string]: any } = useContext(
+    MediumBreakpointContext,
+  );
 
   const dimensions = useWindowDimensions();
 
@@ -19,12 +18,12 @@ const CardContainer = (props: propTypes) => {
     <>
       {specialLayout ? (
         <Link
-          to={`/article/${post.name.replaceAll(" ", "-").toLocaleLowerCase()}`}
+          to={`/article/${post.name.replaceAll(' ', '-').toLocaleLowerCase()}`}
           state={{ post: post }}
           className={`${
             isLowerMediumBreak
-              ? "flex flex-col mb-16"
-              : "grid gap-0 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 xl:gap-0"
+              ? 'flex flex-col mb-16'
+              : 'grid gap-0 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 xl:gap-0'
           } w-9/10`}
         >
           <CardContainerContent
@@ -38,7 +37,7 @@ const CardContainer = (props: propTypes) => {
         </Link>
       ) : (
         <Link
-          to={`/article/${post.name.replaceAll(" ", "-").toLocaleLowerCase()}`}
+          to={`/article/${post.name.replaceAll(' ', '-').toLocaleLowerCase()}`}
           state={{ post: post }}
           key={forKey}
           className="flex flex-col hover:cursor-pointer transition duration-300 rounded-sm ease-in-out w-full md:w-article-item-w-md xl:w-article-item-w-2xl lg:w-article-item-w-lg 3xl:w-article-item-w-3xl sm:h-fit items-start justify-start"
