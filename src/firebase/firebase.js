778541262c89a5b7cb31f045cc7448e5a43b7fed
zmbd <mcypaszh@gmail.com>
2024-11-11 18:54:51 +0200

    remove console logs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,9 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-console.log({processEnv: process.env})
-console.log({firebaseApiKey: process.env.REACT_APP_FIREBASE_API_KEY})
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
